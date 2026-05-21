6cdad08b1dc5c2b143033a07f7dfc485d122df54
Sebastien Caps <493313+virer@users.noreply.github.com>
2026-05-21 09:03:49 +0200

    Adj build tag/push

--- homeserver/radarr/build.sh
+++ homeserver/radarr/build.sh
@@ -10,4 +10,8 @@ mv Radarr*.linux*.tar.gz "radarr.tar.gz"
 
 podman build -f Dockerfile --build-arg "VERSION=${VERSION}" --build-arg "BUILD_VERSION=${BUILD_VERSION}" -t radarr:${VERSION}-${BUILD_VERSION}
 
+
+# podman tag localhost/radarr:${VERSION}-${BUILD_VERSION} quay.io/$QUAY_USER/radarr:${VERSION}-${BUILD_VERSION}
+# podman push quay.io/rh-ee-scaps/radarr:${VERSION}-${BUILD_VERSION}
+
 # EOF
