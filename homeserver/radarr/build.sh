447a5f3e7dd8823fc276c4d94ae3f8d7ccc408b6
Sebastien Caps <493313+virer@users.noreply.github.com>
2026-05-21 09:02:58 +0200

    Adj build ver

--- homeserver/radarr/build.sh
+++ homeserver/radarr/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash 
 ###########
 export VERSION=6.1.1.10360
-export BUILD_VERSION=1
+export BUILD_VERSION=2
 ###########
 set -v
-curl -L -o "radarr.tar.gz" "https://github.com/Radarr/Radarr/releases/download/v${VERSION}/Radarr.master.${VERSION}.linux-core-x64.tar.gz"
+
+wget --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
+mv Radarr*.linux*.tar.gz "radarr.tar.gz"
 
 podman build -f Dockerfile --build-arg "VERSION=${VERSION}" --build-arg "BUILD_VERSION=${BUILD_VERSION}" -t radarr:${VERSION}-${BUILD_VERSION}
 
