cad4dddbb4a95a769717650675ff6c3ec41ce1eb
Sebastien Caps <493313+virer@users.noreply.github.com>
2026-09-17 12:07:23 +0200

    fix rpowlar path

--- homeserver/prowlarr/start.sh
+++ homeserver/prowlarr/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-podman run --name prowlarr --rm -d --network host -v /data/prowlarr:/opt/Prowlarr/.config:z -v /data/dl/:/data/dl/:z quay.io/$QUAY_USER/prowlarr:2.4.0.5397-1 
+podman run --name prowlarr --rm -d --network host -v /data/prowlarr/:/var/lib/prowlarr/:z -v /data/dl/:/data/dl/:z quay.io/$QUAY_USER/prowlarr:2.4.0.5397-1 
 
 # -p 9696:9696
 
